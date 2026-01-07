34c392794555597fbd9a271f2f96ae5f9c7233c3
LiuHaoYang <2302145737@qq.com>
2026-01-08 00:04:18 +0800

    Update hello world with timestamp to force deploy

--- hello/index.js
+++ hello/index.js
@@ -1,6 +1,6 @@
 module.exports = async function (context, req) {
     context.res = {
         status: 200,
-        body: "Hello! If you see this, Azure Functions is working correctly."
+        body: "Hello! If you see this, Azure Functions is working correctly. Timestamp: " + new Date().toISOString()
     };
 };
